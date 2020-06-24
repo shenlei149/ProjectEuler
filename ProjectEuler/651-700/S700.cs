6b4234899a2ea9ec93ed5ef4ecafce199c6d7595
Leo Shen <shenlei149@outlook.com>
2020-06-24 20:37:16 +0800

    S700

---
+++ ProjectEuler/651-700/S700.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectEuler
+{
+    public class S700 : ISolution
+    {
+        public string GetAnswer()
+        {
+            long second = 1504170715041707;
+            long first = 8912517754604;
+            long sum = second + first;
+            while (true)
+            {
+                var next = first - second % first;
+                sum += next;
+                second = first;
+                first = next;
+                if (next == 1 || next == 0)
+                {
+                    break;
+                }
+            }
+
+            return sum.ToString();
+        }
+    }
+}
