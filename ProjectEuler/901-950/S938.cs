6586140513206aae1c46f04fd66270dd3ebcf953
Leo SHEN <shenlei149@outlook.com>
2025-09-16 08:41:42 +0000

    works but not efficient

--- ProjectEuler/901-950/S938.cs
+++ ProjectEuler/901-950/S938.cs
@@ -7,35 +7,50 @@ namespace ProjectEuler
         static readonly int RED = 24690;
         static readonly int BLACK = 12345;
 
-        static readonly double?[,] p = new double?[RED + 1, BLACK + 1];
+        static readonly double?[][] p = new double?[RED + 1][];
 
+        // 0.2928967987
         public string GetAnswer()
         {
+            for (int r = 0; r < RED + 1; r++)
+            {
+                p[r] = new double?[BLACK + 1];
+                for (int b = 0; b < BLACK + 1; b++)
+                {
+                    GetPBy(r, b);
+                }
+
+                if (r > 3)
+                {
+                    p[r - 3] = null;
+                }
+            }
+
             return GetPBy(RED, BLACK).ToString("F10");
         }
 
-        double GetPBy(int red, int black)
+        static double GetPBy(int red, int black)
         {
-            if (p[red, black].HasValue)
+            if (p[red][black].HasValue)
             {
-                return p[red, black].Value;
+                return p[red][black].Value;
             }
 
             if (red == 0 && black == 0)
             {
-                Console.WriteLine("should NOT be here...");
+                return 0;
             }
 
             if (red == 0)
             {
-                p[red, black] = 1;
-                return p[red, black].Value;
+                p[red][black] = 1;
+                return p[red][black].Value;
             }
 
             if (black == 0)
             {
-                p[red, black] = 0;
-                return p[red, black].Value;
+                p[red][black] = 0;
+                return p[red][black].Value;
             }
 
             double p_r_b = GetPBy(red, black - 1);
@@ -56,8 +71,8 @@ namespace ProjectEuler
             }
 
             double p_b_b = (p_r_b * r_b_choice_p + p_r_r * r_r_choice_p) / (1.0 - b_b_choice_p);
-            p[red, black] = p_b_b;
-            return p[red, black].Value;
+            p[red][black] = p_b_b;
+            return p[red][black].Value;
         }
     }
 }
