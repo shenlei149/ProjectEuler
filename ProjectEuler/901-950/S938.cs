b4cf6bd3ed4618b48172f58616d59514f875619a
Leo SHEN <shenlei149@outlook.com>
2025-09-16 07:45:59 +0000

    ok for small size

---
+++ ProjectEuler/901-950/S938.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace ProjectEuler
+{
+    public class S938 : ISolution
+    {
+        static readonly int RED = 24690;
+        static readonly int BLACK = 12345;
+
+        static readonly double?[,] p = new double?[RED + 1, BLACK + 1];
+
+        public string GetAnswer()
+        {
+            return GetPBy(RED, BLACK).ToString("F10");
+        }
+
+        double GetPBy(int red, int black)
+        {
+            if (p[red, black].HasValue)
+            {
+                return p[red, black].Value;
+            }
+
+            if (red == 0 && black == 0)
+            {
+                Console.WriteLine("should NOT be here...");
+            }
+
+            if (red == 0)
+            {
+                p[red, black] = 1;
+                return p[red, black].Value;
+            }
+
+            if (black == 0)
+            {
+                p[red, black] = 0;
+                return p[red, black].Value;
+            }
+
+            double p_r_b = GetPBy(red, black - 1);
+            double r_b_choice_p = 2.0 * red * black / (red + black) / (red + black - 1);
+
+            double p_r_r = 0;
+            double r_r_choice_p = 0;
+            if (red >= 2)
+            {
+                p_r_r = GetPBy(red - 2, black);
+                r_r_choice_p = 1.0 * red * (red - 1) / (red + black) / (red + black - 1);
+            }
+
+            double b_b_choice_p = 0;
+            if (black >= 2)
+            {
+                b_b_choice_p = 1.0 * black * (black - 1) / (red + black) / (red + black - 1);
+            }
+
+            double p_b_b = (p_r_b * r_b_choice_p + p_r_r * r_r_choice_p) / (1.0 - b_b_choice_p);
+            p[red, black] = p_b_b;
+            return p[red, black].Value;
+        }
+    }
+}
